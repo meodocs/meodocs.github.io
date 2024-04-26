8fc27cfe627cab18a9b70d2f66ff9eeec21b04c7
Memory-Studio <sb.nmd@outlook.com>
2024-04-26 22:27:00 +0800

    StarLight

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,46 +3,49 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			customCss: [
-				'./src/style/starlight.css',
-			],
-			title: 'Republic Of Lunar',
-			editLink: {
-				baseUrl: 'https://github.com/lunarlands/lunarlands.github.io/edit/main/',
-			},
-			defaultLocale: 'root',
-			locales: {
-				root: {
-					label: '简体中文',
-					lang: 'zh-CN',
-				},
-			},
-			social: {
-				github: 'https://github.com/lunarlands',
-				gitlab: 'https://gitlab.com/l.r',
-			},
-			sidebar: [
-				{
-					label: "Volume I | Tutorials",
-					items: [
-						{
-							label: 'Chapter I',
-							autogenerate: { directory: 'welcome' },
-						},
-						{
-							label: 'Chapter II',
-							autogenerate: { directory: 'starlight' },
-						},
-						{
-							label: 'Chapter III',
-							badge: 'Updating',
-							autogenerate: { directory: 'arch' },
-						},
-					],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      customCss: ["./src/style/starlight.css"],
+      title: "Republic Of Lunar",
+      editLink: {
+        baseUrl:
+          "https://github.com/lunarlands/lunarlands.github.io/edit/main/",
+      },
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      social: {
+        github: "https://github.com/lunarlands",
+        gitlab: "https://gitlab.com/l.r",
+      },
+      sidebar: [
+        {
+          label: "Volume I | Tutorials",
+          items: [
+            {
+              label: "Chapter I",
+              autogenerate: { directory: "welcome" },
+            },
+            {
+              label: "Chapter II",
+              autogenerate: { directory: "starlight" },
+            },
+            {
+              label: "Chapter III",
+              autogenerate: { directory: "arch" },
+            },
+            {
+              label: "Chapter IV",
+              badge: "Updating",
+              autogenerate: { directory: "greek" },
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
